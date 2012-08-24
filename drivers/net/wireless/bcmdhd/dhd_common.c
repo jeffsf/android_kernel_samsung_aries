f26790633296b3c2614c6f9806fd8e120906243e
Jeff Kletsky <aokp@allycomm.com>
2012-08-24 14:42:24 -0700

    dhd_msg_level * Set in drivers/net/wireless/bcmdhd/dhd_common.c * int dhd_msg_level = DHD_ERROR_VAL | DHD_TRACE_VAL | DHD_INFO_VAL; * Don't let drivers/net/wireless/bcmdhd/wl_android.c re-set
    Change-Id: Id84b7e9e06420efb235b85356f61a9f07030b4f0

--- drivers/net/wireless/bcmdhd/dhd_common.c
+++ drivers/net/wireless/bcmdhd/dhd_common.c
@@ -60,7 +60,8 @@
 #ifdef WLMEDIA_HTSF
 extern void htsf_update(struct dhd_info *dhd, void *data);
 #endif
-int dhd_msg_level = DHD_ERROR_VAL;
+// int dhd_msg_level = DHD_ERROR_VAL;
+int dhd_msg_level = DHD_ERROR_VAL | DHD_TRACE_VAL | DHD_INFO_VAL;
 
 
 #include <wl_iw.h>
